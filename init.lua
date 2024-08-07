d2d60f82804b1c1d5bfc24059d9299c84f550bf3
Ondrej Pilous <ondrej.pilous@t-mobile.cz>
2024-08-07 22:10:10 +0200

    added lazygit + undotree

--- init.lua
+++ init.lua
@@ -277,6 +277,32 @@ require("lazy").setup({
 		},
 	},
 	{
+		"mbbill/undotree",
+		keys = {
+			{ "<leader>tu", "<cmd>UndotreeToggle<cr>", desc = "UndoTree" }
+		}
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>tg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+		}
+	},
+
+	{
 		"neoclide/coc.nvim",
 		branch = "release",
 		config = function()
